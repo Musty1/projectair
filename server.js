e3d4f741586cce07f26722f89851f146024a4377
Musty1 <mustafa.wahidi88@gmail.com>
2017-08-28 02:13:27 +1000

    Comment

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res){
+var mongoose = require('mongoose');
+/*app.get('/', function(req, res){
 	res.send("Project Air");
 })
+*/
 
 app.listen(8051, function(){
 	console.log("Running Server on port : 8051");
 });
+
