43fb5aa88aee104733b0e1dff65562ee7d2acdd1
Musty1 <mustafa.wahidi88@gmail.com>
2017-08-28 02:50:13 +1000

    comment

--- app/models/user.js
+++ app/models/user.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-	username: {type: String, lowercase: true, required: true, unique: true}
-	password: {type: String, required: true}
+	username: {type: String, lowercase: true, required: true, unique: true},
+	password: {type: String, required: true},
 	email: {type: String, lowercase: true, required: true, unique: true}
 });
 
