3f835d452b9efc2680329fa57b501d0311d1ff3d
Musty1 <mustafa.wahidi88@gmail.com>
2017-08-28 02:33:37 +1000

    mongoose

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+
+
 /*app.get('/', function(req, res){
 	res.send("Project Air");
 })
 */
+
+mongoose.connect('mongodb://0.0.0.0:27017/projectair', function(err) {
+	if(err){
+		console.log("Not connected to the Database: " + err);
+	} else  {
+		console.log("Succesfully connected to mongodb");
+	}
+});
+
 
 app.listen(8051, function(){
 	console.log("Running Server on port : 8051");
